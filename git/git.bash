8e53dcf9cde91594d418693ec743914322ccc4a0
Adam Roben <aroben@apple.com>
2017-08-24 16:44:34 -0400

    Switch from PS1 to PROMPT_COMMAND
    Supposedly this is slightly faster.

--- git/git.bash
+++ git/git.bash
@@ -1,4 +1,4 @@
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWSTASHSTATE=1
 GIT_PS1_SHOWUPSTREAM=git
-PS1='\[\e[0;92m\]\W\[\e[m\]$(__git_ps1 " (\[\e[0;93m\]%s\[\e[m\])") \$ '
+PROMPT_COMMAND='__git_ps1 "\[\e[0;92m\]\W\[\e[m\]\[\e[0;93m\]" "\[\e[m\] \$ "'
