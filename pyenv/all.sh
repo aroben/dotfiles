cd5bb1ff20b079832d999fecfabcc081f10064f5
Adam Roben <aroben@apple.com>
2018-05-08 09:16:42 -0400

    Ensure python 3.6.5 is installed

--- pyenv/all.sh
+++ pyenv/all.sh
@@ -3,6 +3,11 @@
 set -eu
 
 cd "$(dirname "$0")"
+
 for plugin in plugins/*; do
   ln -sf "../../$plugin" pyenv.symlink/plugins
 done
+
+if ! ./pyenv.symlink/bin/pyenv versions --bare | grep -q '^3\.6\.5$'; then
+  CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" ./pyenv.symlink/bin/pyenv install 3.6.5
+fi
