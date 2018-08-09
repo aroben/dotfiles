8db2347265102bc164db046c0e9ea50468a523f1
Adam Roben <aroben@apple.com>
2018-08-09 10:31:03 -0400

    Add Tokyo Xcode theme

--- xcode/osx.sh
+++ xcode/osx.sh
@@ -1,2 +1,9 @@
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool YES
 defaults write com.apple.dt.Xcode ExplainWhyBuildCommandsAreRun -bool YES
+
+THIS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+XCODE_THEME_DIR=~/Library/Developer/Xcode/UserData/FontAndColorThemes
+
+mkdir -p "$XCODE_THEME_DIR"
+ln -sf "$THIS_DIR/Tokyo/Tokyo.xccolortheme" "$XCODE_THEME_DIR"
