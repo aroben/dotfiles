e768429e9d25fe1da5810eb4b5d992538dbee392
Adam Roben <adam@roben.org>
2018-09-25 11:34:47 -0400

    Fix pyenv install

--- pyenv/all.sh
+++ pyenv/all.sh
@@ -8,6 +8,9 @@ for plugin in plugins/*; do
   ln -sf "../../$plugin" pyenv.symlink/plugins
 done
 
+export PYENV_ROOT=$(pwd)/pyenv.symlink
+export PATH="$PYENV_ROOT:$PATH"
+
 if ! ./pyenv.symlink/bin/pyenv versions --bare | grep -q '^3\.6\.5$'; then
   CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" ./pyenv.symlink/bin/pyenv install 3.6.5
 fi
--- pyenv/pyenv.bash
+++ pyenv/pyenv.bash
@@ -1,4 +1,5 @@
-export PATH="~/.pyenv/bin:$PATH"
+export PYENV_ROOT=~/.pyenv
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 export PYENV_VERSION=system:3.6.5
